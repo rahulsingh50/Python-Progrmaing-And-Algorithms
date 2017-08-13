bb3314df038af58fb45ec064fc4a03daf573bc41
Rahul <rsembedded@gmail.com>
2017-08-13 17:46:58 +0530

    collection-deque

---
+++ Data Structure/Collections.py
@@ -0,0 +1,13 @@
+'''
+The collections module provides more specialized, high, performance alternatives for
+the built-in data types as well as a utility function to create named tuples.
+
+'''
+
+from collections import deque
+dq =deque('abc')
+dq.append('d')
+dq.appendleft('z')
+dq.extend('efg')
+dq.extendleft('yxw')
+print(dq)

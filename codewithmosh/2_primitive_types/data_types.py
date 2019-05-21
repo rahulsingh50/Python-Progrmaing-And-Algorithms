946fa9114b2542c80a2a89868c4e69fd7d0d1459
rahul <rsembedded@gmail.com>
2019-05-21 19:48:14 +0100

    primtive types

---
+++ codewithmosh/2_primitive_types/data_types.py
@@ -0,0 +1,67 @@
+# primitive data  types
+
+number = 10
+rating = 4.9
+is_published = True
+your_publication = "New york Times"
+
+# string
+book_name = " Machine Learning Python"
+data_science_book = ' Pandas Manipulation '
+message = '''
+
+This is about data type  and 
+String Format
+
+'''
+
+course =  "Python Programming"
+print(len(course))
+print(course[0])
+print(course[-1])
+print(course[0:3])
+print(course[0:])
+print(course[:])
+
+greeting1 = "Good Night "
+print(greeting1)
+greeting2 = 'Good "Morning'
+print(greeting2)
+greeting3 = "Good After Noon\""
+print(greeting3)
+
+
+# String Method
+course = "Python Programing"
+print(course.upper())
+print(course.lower())
+print(course.title)
+print(course.find("pro"))
+print(course.replace("p","j"))
+print(course.strip())
+print(course.lstrip)
+print("pro" in course)
+print("pro" not in course)
+
+
+
+# Numbers
+# 3 types
+number1 = 10 # integer
+number2 = 10.34 # float or decimal
+number = 2 +2j  # Complex
+
+# nUmber functions
+print(round(2.3))
+print(abs(-2.3))
+
+# math module
+import math
+print(math.ceil(2.3))
+
+# False values in python
+# 0
+# ""  Empty string
+# None
+
+

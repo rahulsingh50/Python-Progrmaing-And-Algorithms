064a64d55a2c455e7d040edd359a2919f8642066
Rahul <rsembedded@gmail.com>
2019-07-06 12:32:58 +0530

    python_for_every_body

---
+++ Python_for_every_body/3-boolean.py
@@ -0,0 +1,7 @@
+print(True)
+print(False)
+s = 'Hello World'
+print(dir(s))
+print('heloo' == "hello")
+
+print(30 > 20 and 30 > 20 and 40 > 30)
--- python_crash_cource/chapter-1/1hello_world.py
+++ python_crash_cource/chapter-1/1hello_world.py
@@ -1,1 +1,5 @@
 print("This is Hello World!")
+if 10 > 20:
+    print("Code is Great")
+else:
+    print("Code is looser")

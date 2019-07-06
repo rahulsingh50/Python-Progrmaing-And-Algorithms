a318c3e179cd589efb3aeecf8d29682319a3d63b
Rahul <rsembedded@gmail.com>
2019-07-06 12:44:38 +0530

    python_every_one

---
+++ Python_for_every_body/1-input_function.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the first Number: "))
+b = int(input("Enter the Second Number: "))2
+c = int(input("Enter the Third Number: "))
+
+print("{0} is max number ".format(max(a, b, c)))
+input("Enter any key to Exit")
---
+++ Python_for_every_body/string_test.py
@@ -0,0 +1,5 @@
+s = "hello    "
+y = "This is my dream project"
+
+print(s.lower())
+print(y.split())
